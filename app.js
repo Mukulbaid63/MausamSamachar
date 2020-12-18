03dc57a746469f10df86b2d2c4612988ecec54be
Muddhit Baid <61585314+Mukulbaid63@users.noreply.github.com>
2020-12-19 01:54:38 +0545

    Update app.js

--- app.js
+++ app.js
@@ -1,51 +1,56 @@
-var temp = {
+var api = {
     key: "87cde31000e5170290cebdd94820d23d",
-    base: "https://api.openweathermap.org/data/2.5/"
-  }
-var notificationElement = document.querySelector(".notification");
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(setPosition);
-    notificationElement.innerHTML = "";
+  base :"https://api.openweathermap.org/data/2.5/" 
+}
 
-} 
-else {
-    notificationElement.innerHTML = "Geolocation is not supported by this browser.";
+let latitude,longitude;
+
+
+var searchbox = document.querySelector('.search-box');
+currentlocation()
+
+searchbox.addEventListener('keypress',setQuery);
+
+function currentlocation() {
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(setPosition);
   }
+}
 
 function setPosition(position){
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-
+  latitude = position.coords.latitude;
+  longitude = position.coords.longitude;
+  
   getWeather(latitude, longitude);
 }
-  function getWeather(lat,lon) {
-    fetch(`${temp.base}weather?lat=${lat}&lon=${lon}&units=metric&appid=${temp.key}`)
-      .then(weather => {
-        return weather.json();
-      }).then(displayResults);
-  }
 
-  function displayResults (weather) {
-    let icon=document.querySelector('.icon-1');
-    icon.innerHTML=`<img src="${weather.weather[0].icon}.png"/>`;
-    let humidity=document.querySelector('.humidity-value');
-    humidity.innerHTML=`${weather.main.humidity}`
-    icon.setAttribute("src","")
-    let city = document.querySelector('.location .city');
-    city.innerText = `${weather.name}, ${weather.sys.country}`;
+function getWeather(lat,lon) {
+  fetch(`${api.base}weather?lat=${lat}&lon=${lon}&units=metric&appid=${api.key}`)
+    .then(weather => {
+      return weather.json();
+    }).then(displayResults);
+}
 
-    let temp = document.querySelector('.current .temp');  
-    temp.innerHTML =`${Math.round(weather.main.temp)}<span>°C</span>`
+function displayResults(weather) {
+  let city=document.querySelector('.city')
+ city.innerHTML= `${weather.name}, ${weather.sys.country}`;
+ 
+ let temp = document.querySelector('.current .temp');
+ temp.innerHTML = `${Math.round(weather.main.temp)}°c`;
 
-    let weather_el = document.querySelector('.current .weather');
-    weather_el.innerText = weather.weather[0].main;
+ let weather_el = document.querySelector('.current .weather');
+ weather_el.innerHTML = weather.weather[0].main;
 
-    let wind=document.querySelector('.wind-speed');
-    wind.innerText=weather.wind.speed;
+ 
+ let icon=document.querySelector('.icon-1');
+ icon.innerHTML=`<img src="${weather.weather[0].icon}.png"/>`;
+ let humidity=document.querySelector('.humidity-value');
+ humidity.innerHTML=`${weather.main.humidity}`
+ let wind=document.querySelector('.wind-speed');
+ wind.innerText=weather.wind.speed;
 
-    let deg=document.querySelector('.wind-degree');
-    deg.innerText=weather.wind.deg;
-
-  }
-
+ let deg=document.querySelector('.wind-degree');
+ deg.innerText=weather.wind.deg;
+ 
+}
 
